f1108faaa99419f5cd79d7eaf633c8986eb2e00b
Luis Alberto Canizares (Alberto) <canizarl@tcd.ie>
2020-08-05 14:33:07 +0100

    monitor.sh output changed to show in data.lofar.ie

--- monitor.sh
+++ monitor.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 "
  ILOFAR MONITOR
 
@@ -30,61 +31,78 @@
 "
 
 
-
-
 while true
 do
-        source activate monitor_env
+	source activate monitor_env
 
-        today=$(date +'%Y.%m.%d')
+	today=$(date +'%Y.%m.%d')
 
-        mkdir -p  ~/Monitor/data_buff/$today
+	mkdir -p  ~/Monitor/data_buff/$today
 
-        # # # # # # # # # #
-        # Directories     #
-        # # # # # # # # # #
-        og_data_source="lcu:/data/home/user1/data/kbt/rcu357_1beam/${today}/*00?.dat"
-        realta_data_source="lcu:/data/home/user1/data/kbt/rcu357_1beam_datastream/${today}/*00?.dat"
-        monitor_temp_data=~/"Monitor/data_buff/${today}"
-        python_dir=~/"Scripts/Python/MonitorRealtime/"
-
-
-
-        echo " "
-        echo $(date)
+	# # # # # # # # # #
+	# Directories     #
+	# # # # # # # # # #
+	og_data_source="lcu:/data/home/user1/data/kbt/rcu357_1beam/${today}/*00?.dat"
+	realta_data_source="lcu:/data/home/user1/data/kbt/rcu357_1beam_datastream/${today}/*00?.dat"
+	monitor_temp_data=~/"Monitor/data_buff/${today}"
+	python_dir=~/"Scripts/Python/MonitorRealtime/"
+	YYYY=$(date +'%Y')
+	MM=$(date +'%m')
+	DD=$(date +'%d')
 
 
+	echo " "
+	echo $(date)
 
-        if rsync -ahP $og_data_source $monitor_temp_data | grep -q '.dat'; then
-                echo "Upload succeeded" 
-                python /home/ilofar/Scripts/Python/MonitorRealtime/lofar_monitor.py ${monitor_temp_data}/*X*
-                sensors > status_lgc.txt
-                ssh lcu 'rspctl --status' > status_lcu.txt
+
+
+	if rsync -ahP $og_data_source $monitor_temp_data | grep -q '.dat'; then
+		echo "Upload succeeded"
+                python /home/ilofar/Scripts/Python/MonitorRealtime/lofar_monitor.py ${monitor_temp_data}/*X* /home/ilofar/Data/IE613/monitor/$today
+		sensors > status_lgc.txt
+		ssh lcu 'rspctl --status' > status_lcu.txt
                 echo "lofar monitor generated a preview."
                 echo ""
-        elif rsync -ahP $realta_data_source $monitor_temp_data/datastream | grep -q '.dat'; then
-                echo "Upload succeeded. Realta is working." 
-                python /home/ilofar/Scripts/Python/MonitorRealtime/lofar_monitor.py ${monitor_temp_data}/datastream/*X*
+	elif rsync -ahP $realta_data_source $monitor_temp_data/datastream | grep -q '.dat'; then
+		echo "Upload succeeded. Realta is working."
+                python /home/ilofar/Scripts/Python/MonitorRealtime/lofar_monitor.py ${monitor_temp_data}/datastream/*X* /home/ilofar/Data/IE613/monitor/$today
                 sensors > status_lgc.txt
-                ssh lcu 'rspctl --status' > status_lcu.txt
-                echo "lofar monitor generated a preview."
+		ssh lcu 'rspctl --status' > status_lcu.txt
+		echo "lofar monitor generated a preview."
                 echo ""
-        else
-                sensors > status_lgc.txt
-                echo "LCU IN INTERNATIONAL MODE" > status_lcu.txt
-                echo "Upload failed. Try in half an hour"
-                sleep 1200
-        fi
+	else
+		sensors > status_lgc.txt
+		echo "LCU IN INTERNATIONAL MODE" > status_lcu.txt
+    		echo "Upload failed. Try in half an hour"
+		sleep 1200
+	fi
 
 
 
-        # send logs
-        echo "sending logs to webserver"
-        curl -F file=@status_lcu.txt https://lofar.ie/monitor/post_log.php?txt=status_lcu
+
+
+
+
+	# send logs
+	echo "sending logs to webserver"
+	curl -F file=@status_lcu.txt https://lofar.ie/monitor/post_log.php?txt=status_lcu
         curl -F file=@status_lgc.txt https://lofar.ie/monitor/post_log.php?txt=status_lgc
-        echo "logs sent correctly"      
+	echo "logs sent correctly"
 
-        echo " sleep 10 mins"
-        #wait 10 mins 
-        sleep 600
+	echo " sleep 10 mins"
+	#wait 10 mins
+	sleep 600
 done
+
+
+
+
+
+
+
+
+
+
+
+
+
